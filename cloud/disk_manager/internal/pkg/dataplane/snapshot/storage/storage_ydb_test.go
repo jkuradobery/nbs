bccc26482be6b654c657e1e26b4e875f69a6af62
s-vorobe-v <sergei.vorobev@nebius.com>
2024-02-02 15:50:24 +0100

    Revert context

--- cloud/disk_manager/internal/pkg/dataplane/snapshot/storage/storage_ydb_test.go
+++ cloud/disk_manager/internal/pkg/dataplane/snapshot/storage/storage_ydb_test.go
@@ -840,7 +840,7 @@ func TestShallowCopySnapshotWithRandomFailure(t *testing.T) {
 					attemptIndex,
 					milestoneChunkIndex,
 				)
-				copyCtx, cancelCopyCtx := context.WithCancel(context.Background())
+				copyCtx, cancelCopyCtx := context.WithCancel(test.NewContext())
 				defer cancelCopyCtx()
 
 				rand.Seed(time.Now().UnixNano())
